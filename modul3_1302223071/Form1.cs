da5e91a00193d1b72c5d5cf206d503042c6ce6bd
unknown <aldinorasend@gmail.com>
2024-03-06 07:43:08 +0700

    menambahkan implementasi kalkulator

--- modul3_1302223071/Form1.cs
+++ modul3_1302223071/Form1.cs
@@ -6,5 +6,104 @@ namespace modul3_1302223071
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            label_output.Text += "1";
+            
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            label_output.Text += "2";
+            
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            label_output.Text += "3";
+            
+
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            label_output.Text += "4";
+           
+
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            label_output.Text += "5";
+
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            label_output.Text += "6";
+            
+
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            label_output.Text += "7";
+
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            label_output.Text += "8";
+
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            label_output.Text += "9";
+
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            label_output.Text += "0";
+
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+           
+            clickcounter++;
+            if(clickcounter == 1)
+            {
+                num1 = int.Parse(label_output.Text);
+               
+
+            }
+            else if (clickcounter == 2)
+            {
+                num3 = int.Parse(label_output.Text);
+
+            }
+            label_output.Text = null;
+           
+
+        }
+
+        private void btnResult_Click(object sender, EventArgs e)
+        {
+           num2 = int.Parse(label_output.Text);
+           int result = num1 + num2 + num3;
+           label_output.Text = result.ToString();
+        }
+        int num1,num2,num3;
+        int clickcounter = 0;
+
     }
 }
--- modul3_1302223071/obj/Debug/net8.0-windows/modul3_1302223071.AssemblyInfo.cs
+++ modul3_1302223071/obj/Debug/net8.0-windows/modul3_1302223071.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul3_1302223071")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0e0c719dec0c7fba2be7176f912c981990253dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul3_1302223071")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul3_1302223071")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
